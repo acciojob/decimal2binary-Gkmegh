c4345726062c83de6debee6be19f2f73f74986ce
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-06-26 10:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function threeSum(arr, target) {
-// write your code here
-let n = arr;
-console.log(n.toString(target));
+function decimalToBinary(decimal) {
+    let binary = "";
+    while (decimal > 0) {
+        if (decimal % 2 == 1) {
+            binary = "1" + binary;
+        } else {
+            binary = "0" + binary;
+        }
+        decimal = Math.floor(decimal / 2);
+    }
+    return binary;
 }
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
